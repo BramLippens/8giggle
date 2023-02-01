61e00ad0f62b06b70dbbee38c0ebc46425f50a98
BramLippens <79507864+BramLippens@users.noreply.github.com>
2023-02-01 01:25:21 +0100

    added mock categories

---
+++ frontend-8giggle/src/api/mock-data.js
@@ -0,0 +1,28 @@
+const CATEGORY_DATA = [
+  {
+    id: "1",
+    name: "Anime",
+  },
+  {
+    id: "2",
+    name: "News",
+  },
+  {
+    id: "3",
+    name: "Cats",
+  },
+  {
+    id: "4",
+    name: "Crypto",
+  },
+  {
+    id: "5",
+    name: "Games",
+  },
+  {
+    id: "6",
+    name: "Movies",
+  },
+];
+
+export { CATEGORY_DATA };
