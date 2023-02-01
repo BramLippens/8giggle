3dcd391309148a903cc0200306a9095fa3fb5721
BramLippens <79507864+BramLippens@users.noreply.github.com>
2023-02-01 01:25:45 +0100

    implemented category

---
+++ frontend-8giggle/src/components/Category/CategoryList.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { CATEGORY_DATA } from "../../api/mock-data";
+import { Category } from "../../Category";
+
+export const CategoryList = () => {
+  const [categories, setCategories] = useState(CATEGORY_DATA);
+  return (
+    <div className="px-2">
+      <div className="flex -mx-2">
+        {categories.map((category) => (
+          <div className="px-2">
+            <Category key={category.id} name={category.name} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
