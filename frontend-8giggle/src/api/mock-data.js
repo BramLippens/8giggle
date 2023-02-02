e08a9751e0dda8f804b60e33322061ff91a24cbb
BramLippens <79507864+BramLippens@users.noreply.github.com>
2023-02-02 02:22:43 +0100

    Added mock posts

--- frontend-8giggle/src/api/mock-data.js
+++ frontend-8giggle/src/api/mock-data.js
@@ -24,5 +24,34 @@ const CATEGORY_DATA = [
     name: "Movies",
   },
 ];
+const POST_DATA = [
+  {
+    id: "1",
+    title: "First Post",
+    image: "https://source.unsplash.com/random/640x800",
+    author: "1",
+    likes: "4",
+    dislikes: "1",
+    categories: ["latest", "awesome", "satisfying"],
+  },
+  {
+    id: "2",
+    title: "Second Post",
+    image: "https://source.unsplash.com/random/640x800",
+    author: "2",
+    likes: "3",
+    dislikes: "2",
+    categories: ["latest", "awesome", "satisfying"],
+  },
+  {
+    id: "3",
+    title: "Third Post",
+    image: "https://source.unsplash.com/random/640x800",
+    author: "3",
+    likes: "2",
+    dislikes: "1",
+    categories: ["latest", "awesome", "satisfying"],
+  },
+];
 
-export { CATEGORY_DATA };
+export { CATEGORY_DATA, POST_DATA };
