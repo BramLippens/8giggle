59e15347736fac15ff56fdd8535f9e6dcc0a87bd
BramLippens <79507864+BramLippens@users.noreply.github.com>
2023-02-15 23:03:05 +0100

    create login and register route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// Routes
+app.get("/", require("./routes/login"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ api/routes/login.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { registerView, loginView } = require("../controllers/loginController");
+
+router.get("/register", registerView);
+router.get("/login", loginView);
+
+module.exports = router;
