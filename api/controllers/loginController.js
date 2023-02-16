a0ea522d46a226beeeb5c410da9909e09c8ce117
BramLippens <79507864+BramLippens@users.noreply.github.com>
2023-02-16 11:58:52 +0100

    Connection mongoDB and user schema

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -1,5 +1,44 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 const registerView = (req, res) => {
-  res.render("register", {});
+  const { username, password, password2, email } = req.body;
+  let errors = [];
+
+  if (!username || !password || !password2 || !email) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+
+  if (password !== password2) {
+    errors.push({ msg: "Passwords do not match" });
+  } else {
+    User.findOne({ email: email, username: username }).then((user) => {
+      if (user) {
+        errors.push({ msg: "email already exists" });
+      } else {
+        const newUser = new User({
+          username,
+          password,
+          email,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => {
+                res.redirect("/");
+              })
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    });
+  }
+  // log errors
+  console.log(errors);
 };
 
 const loginView = (req, res) => {
--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const database = process.env.MONGOLAB_URI;
+mongoose
+  .connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
 app.set("view engine", "ejs");
 
 const port = process.env.PORT || 3000;
